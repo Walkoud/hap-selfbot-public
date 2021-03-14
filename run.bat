bf0048f30bab98e863321e680aa1ef9466e2d4f4
Walkoud <38588921+Walkoud@users.noreply.github.com>
2021-03-14 13:35:51 +0100

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,1 +1,12 @@
-start cmd.exe
+@echo off
+
+
+set l1 = "HAP SELFBOT"
+set l2 = "_______________________"
+set l3 = USE "node index help" to change configuration
+set l4 = UTILSIEZ "node index help" pour changer la config
+set l5 = _______________________
+set l6 = TO START SELFBOT "node index"
+set l7 = POUR LANCER LE SELFBOT "node index"
+
+start "TEST" cmd.exe /k "TITLE TEST & color 0A &echo HAP SELFBOT & echo _______________________ & echo USE "node index help" to change configuration & echo UTILSIEZ "node index help" pour changer la config & echo _______________________ & echo TO START SELFBOT "node index" & echo POUR LANCER LE SELFBOT "node index" & echo %l8%"
